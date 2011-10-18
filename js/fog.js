8ed4848f07b4e5c44c5ec27430e54a89a69ef5f1
Cedric Pinson <cedric.pinson@plopbyte.com>
2011-10-18 16:02:17 +0200

    Update music Change value of fog range

--- js/fog.js
+++ js/fog.js
@@ -7,7 +7,7 @@ var getFogFragmentCode = function() {
     var fragmentshader = [
 
         "vec4 fog3(vec4 inputColor) {",
-        "  float fogfar = 1.0 - smoothstep(500.0,700.0, length(worldPosition));",
+        "  float fogfar = 1.0 - smoothstep(500.0,600.0, length(worldPosition));",
         "  float value = fogfar;",
         "  vec3 fogColor = vec3(1.0);",
         "  vec4 color = mix(vec4(fogColor,1.0), inputColor, value);",
